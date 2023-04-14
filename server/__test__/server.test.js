7efb087d7a1061de59f9012331b62959d58b1bc7
PM Háry Dávid <102281703+pmharydavid@users.noreply.github.com>
2023-04-14 14:49:41 +0200

    Update server.test.js

--- server/__test__/server.test.js
+++ server/__test__/server.test.js
@@ -5,7 +5,7 @@ describe('Testing home page', ()=> {
 
     test('Response return with 200 status code', async()=> {
         const res = await supertest(app).get('/');
-        expect(res.statusCode).toBe(300)
+        expect(res.statusCode).toBe(200)
     })
 
     test('All path return 200 code and html content type', async ()=> {
